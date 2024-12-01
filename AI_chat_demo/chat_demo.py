2027d93b748e5d99baed3b1d88e5db48a7a42056
Leon <2594041827@qq.com>
2024-12-01 15:45:51 +0800

    更新chat_demo.py，移除注释代码并修改API Key设置

--- AI_chat_demo/chat_demo.py
+++ AI_chat_demo/chat_demo.py
@@ -1,26 +1,8 @@
-# import os
-# from volcenginesdkarkruntime import Ark
-
-# # 设置API Key
-# client = Ark(api_key=os.environ.get("055d8443-2a1e-4e9c-b82c-9abc67f6f435"))
-
-# print("----- multiple rounds request -----")
-# completion = client.chat.completions.create(
-#     model="ep-20241130154059-sj7ld",
-#     messages = [
-#         {"role": "system", "content": "你是豆包，是由字节跳动开发的 AI 人工智能助手"},
-#         {"role": "user", "content": "花椰菜是什么？"},
-#         {"role": "assistant", "content": "花椰菜又称菜花、花菜，是一种常见的蔬菜。"},
-#         {"role": "user", "content": "再详细点"},
-#     ],
-# ) 
-# print(completion.choices[0].message.content)
-
 
 import os
 from volcenginesdkarkruntime import Ark
 
-# 设置API Key
+# client = Ark(api_key=os.environ.get("055d8443-2a1e-4e9c-b82c-9abc67f6f435"))
 client = Ark(api_key=os.environ.get("ARK_API_KEY"))
 
 print("----- multiple rounds request -----")
